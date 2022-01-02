0862aba9a913933d49a9d7f75c5ce6ec82388066
lukaw3d <lukaw3d@gmail.com>
2022-01-02 02:13:53 +0100

    Initialize vitepress

---
+++ README.md
@@ -0,0 +1,1 @@
+[blog/pages](./blog/pages/)
---
+++ blog/.vitepress/config.js
@@ -0,0 +1,8 @@
+/** @type {import('vitepress').UserConfig} */
+const config = {
+  title: 'lukaw3d random notes / blog',
+  description: 'typescript, vue, misc',
+  base: '/blog'
+};
+
+module.exports = config;
